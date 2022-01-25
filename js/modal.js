a5f08ad0373599e6b679de8041dbf4f460c277c3
Sabrina-a <sabrina.aboelfetooh@gmail.com>
2022-01-25 02:00:34 +0200

    commit

--- js/modal.js
+++ js/modal.js
@@ -15,7 +15,7 @@ function addData(e) {
 <p class="hearth"></p>
 </label>
 </td>
-<td><button id='close' >close</button></td>
+<td><button id='close' onclick="removeTodo()" >close</button></td>
 <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
 <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
 <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
@@ -41,12 +41,12 @@ window.addEventListener("click", (e) =>
 add_data.addEventListener("click", addData);
 
 //remove todo
-function removeTodo(btn) {
-  console.log("btn", btn);
-  var row = btn.parentNode.parentNode;
-  row.parentNode.removeChild(row);
+function removeTodo(e) {
+  var row = e.target;
+  console.log(row);
+  // row.parentNode.removeChild(row);
 }
 
-close.addEventListener("click", function () {
-  removeTodo(btn);
-});
+// close.addEventListener("click", function () {
+//   removeTodo(btn);
+// });
